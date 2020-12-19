d8fa65b2bf90d34bd2ff088eb1ecb19c2b134cea
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-12-20 03:21:37 +0530

    changes in Item Component

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ export const Item = ({ item }) => {
   const { image_url, title } = item;
   return (
     <div className="item">
-      <img src={image_url} alt="image" />
+      <img src={image_url} alt="monster" />
       <div className="item-title">
         <h5>{title}</h5>
       </div>
